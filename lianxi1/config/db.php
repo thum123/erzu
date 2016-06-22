a415d272787600a693a18e567e9444ced2b25b50
wb <124@qq.com>
2016-06-22 09:00:29 +0800

    del

--- lianxi1/config/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-							return [
-							    'class' => 'yii\db\Connection',
-							    'dsn' => 'mysql:host=127.0.0.1;dbname=www',
-							    'username' => 'root',
-							    'password' => 'root',
-							    'charset' => 'utf8',
-							];
--- lianxi1/pdo.php
+++
@@ -1,4 +0,0 @@
-<?php
-						$pdo = new PDO("mysql:host=localhost;dbname=www","root","root");
-						$pdo -> exec("set names utf8");
-						
